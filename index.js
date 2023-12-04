7cf973470570b23817306e5f657a07ab4aecca9c
Filip Kin <me@filipkin.com>
2023-12-04 16:20:10 -0500

    Allow specifying the command

--- index.js
+++ index.js
@@ -6,104 +6,107 @@ const { spawn } = require('child_process')
 const recorders = require('./recorders')
 
 class Recording {
-  constructor (options = {}) {
-    const defaults = {
-      sampleRate: 16000,
-      channels: 1,
-      compress: false,
-      threshold: 0.5,
-      thresholdStart: null,
-      thresholdEnd: null,
-      silence: '1.0',
-      recorder: 'sox',
-      endOnSilence: false,
-      audioType: 'wav'
+    constructor(options = {}) {
+        const defaults = {
+            cmd: 'sox',
+            sampleRate: 16000,
+            channels: 1,
+            compress: false,
+            threshold: 0.5,
+            thresholdStart: null,
+            thresholdEnd: null,
+            silence: '1.0',
+            recorder: 'sox',
+            endOnSilence: false,
+            audioType: 'wav'
+        }
+
+        if (!options.hasOwnProperty('cmd') && options.hasOwnProperty('recorder')) options.cmd = options.recorder;
+        this.options = Object.assign(defaults, options)
+
+        const recorder = recorders.load(this.options.recorder)
+        const { args, spawnOptions = {} } = recorder(this.options)
+        const cmd = options.cmd;
+
+        this.cmd = cmd
+        this.args = args
+        this.cmdOptions = Object.assign({ encoding: 'binary', stdio: 'pipe' }, spawnOptions)
+
+        debug(`Started recording`)
+        debug(this.options)
+        debug(` ${this.cmd} ${this.args.join(' ')}`)
+
+        return this.start()
     }
 
-    this.options = Object.assign(defaults, options)
+    start() {
+        const { cmd, args, cmdOptions } = this
 
-    const recorder = recorders.load(this.options.recorder)
-    const { cmd, args, spawnOptions = {} } = recorder(this.options)
+        const cp = spawn(cmd, args, cmdOptions)
+        const rec = cp.stdout
+        const err = cp.stderr
 
-    this.cmd = cmd
-    this.args = args
-    this.cmdOptions = Object.assign({ encoding: 'binary', stdio: 'pipe' }, spawnOptions)
+        this.process = cp // expose child process
+        this._stream = rec // expose output stream
 
-    debug(`Started recording`)
-    debug(this.options)
-    debug(` ${this.cmd} ${this.args.join(' ')}`)
-
-    return this.start()
-  }
-
-  start () {
-    const { cmd, args, cmdOptions } = this
-
-    const cp = spawn(cmd, args, cmdOptions)
-    const rec = cp.stdout
-    const err = cp.stderr
-
-    this.process = cp // expose child process
-    this._stream = rec // expose output stream
-
-    cp.on('close', code => {
-      if (code === 0) return
-      rec.emit('error', `${this.cmd} has exited with error code ${code}.
+        cp.on('close', code => {
+            if (code === 0) return
+            rec.emit('error', `${this.cmd} has exited with error code ${code}.
 
 Enable debugging with the environment variable DEBUG=record.`
-      )
-    })
+            )
+        })
 
-    err.on('data', chunk => {
-      debug(`STDERR: ${chunk}`)
-    })
+        err.on('data', chunk => {
+            debug(`STDERR: ${chunk}`)
+        })
 
-    rec.on('data', chunk => {
-      debug(`Recording ${chunk.length} bytes`)
-    })
+        rec.on('data', chunk => {
+            debug(`Recording ${chunk.length} bytes`)
+        })
 
-    rec.on('end', () => {
-      debug('Recording ended')
-    })
+        rec.on('end', () => {
+            debug('Recording ended')
+        })
 
-    return this
-  }
+        return this
+    }
 
-  stop () {
-    assert(this.process, 'Recording not yet started')
+    stop() {
+        assert(this.process, 'Recording not yet started')
 
-    this.process.kill()
-  }
+        this.process.kill()
+    }
 
-  pause () {
-    assert(this.process, 'Recording not yet started')
+    pause() {
+        assert(this.process, 'Recording not yet started')
 
-    this.process.kill('SIGSTOP')
-    this._stream.pause()
-    debug('Paused recording')
-  }
+        this.process.kill('SIGSTOP')
+        this._stream.pause()
+        debug('Paused recording')
+    }
 
-  resume () {
-    assert(this.process, 'Recording not yet started')
+    resume() {
+        assert(this.process, 'Recording not yet started')
 
-    this.process.kill('SIGCONT')
-    this._stream.resume()
-    debug('Resumed recording')
-  }
+        this.process.kill('SIGCONT')
+        this._stream.resume()
+        debug('Resumed recording')
+    }
 
-  isPaused () {
-    assert(this.process, 'Recording not yet started')
+    isPaused() {
+        assert(this.process, 'Recording not yet started')
 
-    return this._stream.isPaused()
-  }
+        return this._stream.isPaused()
+    }
 
-  stream () {
-    assert(this._stream, 'Recording not yet started')
+    stream() {
+        assert(this._stream, 'Recording not yet started')
 
-    return this._stream
-  }
+        return this._stream
+    }
 }
 
 module.exports = {
-  record: (...args) => new Recording(...args)
+    record: (...args) => new Recording(...args)
 }
